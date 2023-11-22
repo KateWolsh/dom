61d5bb617e491411eae014d9c0c07a1cfd9954bf
Kate <katewolshanat@gmail.com>
2023-11-22 12:34:45 +0100

    added logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,66 +1,17 @@
 // TODO: write code here
-// document.addEventListener("DOMContentLoaded", function () {
-//   const gameBoard = document.querySelector(".game-board");
-//   const monster = document.querySelector(".monster");
-
-//   // Функция для генерации случайной позиции внутри игрового поля
-//   function getRandomCell() {
-//     const cells = gameBoard.getElementsByClassName("cell");
-//     const randomCell = cells[Math.floor(Math.random() * cells.length)];
-//     return randomCell;
-//   }
-//   // Функция для установки монстра в случайную позицию
-//   function setMonsterRandomPosition() {
-//     const randomCell = getRandomCell();
-//     randomCell.appendChild(monster);
-//   }
-//   // Вызываем функцию для установки монстра на случайную позицию при загрузке страницы
-//   setMonsterRandomPosition();
-//   // Запускаем перемещение монстра с интервалом
-//   setInterval(setMonsterRandomPosition, 1000);
-// });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   class Game {
-//     constructor() {
-//       this.gameBoard = document.getElementsByClassName("game-board")[0];
-//       this.monster = document.getElementsByClassName("monster")[0];
-
-//       this.init();
-//     }
-
-//     getRandomCell() {
-//       const cells = this.gameBoard.getElementsByClassName("cell");
-//       const randomCell = cells[Math.floor(Math.random() * cells.length)];
-//       return randomCell;
-//     }
-
-//     setMonsterRandomPosition() {
-//       const randomCell = this.getRandomCell();
-//       randomCell.appendChild(this.monster);
-//     }
-
-//     init() {
-//       this.setMonsterRandomPosition();
-//       setInterval(() => this.setMonsterRandomPosition(), 1000);
-//     }
-//   }
-
-//   const gameInstance = new Game();
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
   class GameLogic {
     constructor(game) {
       this.game = game;
       this.score = 0;
-      this.missedAttempts = 0; // Новое свойство для отслеживания промахов
+      this.missedAttempts = 0;
 
       this.scoreDisplay = document.querySelector(".score");
       this.game.monster.addEventListener("click", () =>
         this.handleMonsterClick()
       );
-      this.initGameBoardClickHandler(); // Добавим обработчик для кликов по ячейкам
+      this.initGameBoardClickHandler();
+      this.initMonsterHover();
       this.updateScoreDisplay();
     }
 
@@ -78,19 +29,15 @@ document.addEventListener("DOMContentLoaded", function () {
     handleMonsterClick() {
       this.score += 1;
       this.updateScoreDisplay();
-      this.resetMissedAttempts(); // Сбрасываем количество промахов
+      this.resetMissedAttempts();
 
-      // Убираем монстра из текущей ячейки
       this.game.monster.parentNode.removeChild(this.game.monster);
-
-      // Устанавливаем монстра в новую случайную позицию
       this.setMonsterRandomPosition();
     }
 
     handleGameBoardClick() {
       this.missedAttempts += 1;
 
-      // Проверяем, не промахнулся ли игрок 5 раз подряд
       if (this.missedAttempts >= 5) {
         this.endGame();
       }
@@ -103,10 +50,19 @@ document.addEventListener("DOMContentLoaded", function () {
     initGameBoardClickHandler() {
       const cells = this.game.gameBoard.getElementsByClassName("cell");
 
-      // Добавляем обработчик клика для каждой ячейки
       for (let i = 0; i < cells.length; i++) {
         cells[i].addEventListener("click", () => this.handleGameBoardClick());
       }
+    }
+
+    initMonsterHover() {
+      this.game.monster.addEventListener("mouseover", () => {
+        this.game.monster.style.cursor = "pointer"; // Используем встроенный курсор pointer
+      });
+
+      this.game.monster.addEventListener("mouseout", () => {
+        this.game.monster.style.cursor = "auto";
+      });
     }
 
     updateScoreDisplay() {
@@ -115,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     endGame() {
       alert("Game Over! Your final score is: " + this.score);
-      location.reload(); // Перезагружаем страницу после окончания игры
+      location.reload();
     }
   }
 
@@ -137,13 +93,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const gameInstance = new Game();
 });
 
-
 // comment this to pass build
 // const unusedVariable = "variable";
 
 // for demonstration purpose only
-export default function demo(value) {
-  return `Demo: ${value}`;
-}
+// export default function demo(value) {
+//   return `Demo: ${value}`;
+// }
 
-console.log("app.js included");
+// console.log("app.js included");
