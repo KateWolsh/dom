09bd706c8a533e6b267c74ec36394f94c8c19eb1
Kate <katewolshanat@gmail.com>
2024-02-02 12:20:32 +0100

    added classes

--- src/js/app.js
+++ src/js/app.js
@@ -1,121 +1,84 @@
-// TODO: write code here
-
-window.addEventListener("DOMContentLoaded", function () {
-  // Класс, отвечающий за логику игры
-  class GameLogic {
-    constructor(game) {
-      this.game = game;
-      this.score = 0;
-      this.missedAttempts = 0;
-
-      this.scoreDisplay = document.querySelector(".score");
-      this.game.monster.addEventListener("click", () =>
-        this.handleMonsterClick()
-      );
-      this.initGameBoardClickHandler();
-      this.initMonsterHover();
-      this.updateScoreDisplay();
-      this.startMonsterInterval();
-    }
-
-    // Получение случайной ячейки из игрового поля
-    getRandomCell() {
-      const cells = this.game.gameBoard.getElementsByClassName("cell");
-      const randomCell = cells[Math.floor(Math.random() * cells.length)];
-      return randomCell;
-    }
-
-    // Установка гоблина в случайную позицию на игровом поле
-    setMonsterRandomPosition() {
-      const randomCell = this.getRandomCell();
-      randomCell.appendChild(this.game.monster);
-    }
-
-    // Обработка клика по гоблину
-    handleMonsterClick() {
-      this.score += 1;
-      this.updateScoreDisplay();
-      this.resetMissedAttempts();
-    }
-
-    // Обработка клика по игровому полю
-    handleGameBoardClick() {
-      this.missedAttempts += 1;
-
-      if (this.missedAttempts >= 5) {
-        this.endGame();
-      }
-    }
-
-    // Сброс счетчика промахов
-    resetMissedAttempts() {
-      this.missedAttempts = 0;
-    }
-
-    // Инициализация обработчика клика для каждой ячейки игрового поля
-    initGameBoardClickHandler() {
-      const cells = this.game.gameBoard.getElementsByClassName("cell");
-
-      for (let i = 0; i < cells.length; i++) {
-        cells[i].addEventListener("click", () => this.handleGameBoardClick());
-      }
-    }
-
-    // Инициализация обработчика наведения для гоблина
-    initMonsterHover() {
-      this.game.monster.addEventListener("mouseover", () => {
-        this.game.monster.style.cursor = "pointer"; // Используем встроенный курсор pointer
-      });
-
-      this.game.monster.addEventListener("mouseout", () => {
-        this.game.monster.style.cursor = "auto";
-      });
-    }
-
-    // Запуск интервала для перемещения гоблина
-    startMonsterInterval() {
-      // Используем setTimeout с рекурсивным вызовом
-      const moveMonster = () => {
-        this.setMonsterRandomPosition();
-        setTimeout(moveMonster, 1000);
-      };
-
-      // Запускаем первый вызов интервала
-      moveMonster();
-    }
-
-    // Обновление отображения счета
-    updateScoreDisplay() {
-      this.scoreDisplay.textContent = "Score: " + this.score;
-    }
-
-    // Завершение игры
-    endGame() {
-      alert("Game Over! Your final score is: " + this.score);
-      location.reload();
-    }
+// Определение класса Goblin
+class Goblin {
+  constructor(element) {
+    this.element = element;
+    this.initHover();
   }
 
-  // Класс, представляющий саму игру
-  class Game {
-    constructor() {
-      this.gameBoard = document.getElementsByClassName("game-board")[0];
-      this.monster = document.getElementsByClassName("monster")[0];
+  // Инициализация обработчика наведения на гоблина
+  initHover() {
+    this.element.addEventListener("mouseover", () => {
+      this.element.style.cursor = "pointer"; // Используем встроенный курсор pointer
+    });
 
-      this.logic = new GameLogic(this);
-    }
+    this.element.addEventListener("mouseout", () => {
+      this.element.style.cursor = "auto";
+    });
   }
-
-  // Создание экземпляра игры
-  const gameInstance = new Game();
-});
-
-// const unusedVariable = "variable";
-
-// Экспорт для демонстрационных целей
-export default function demo(value) {
-  return `Demo: ${value}`;
 }
 
-// Вывод в консоль для проверки подключения файла
-console.log("app.js included");
+// Определение класса Field
+class Field {
+  constructor(cells) {
+    this.cells = cells;
+    this.initClickHandlers();
+  }
+
+  // Инициализация обработчиков клика для ячеек поля
+  initClickHandlers() {
+    for (let i = 0; i < this.cells.length; i++) {
+      this.cells[i].addEventListener("click", () => this.handleCellClick());
+    }
+  }
+}
+
+// Определение класса GameLogic
+class GameLogic {
+  constructor(field, goblin, scoreDisplay) {
+    this.field = field;
+    this.goblin = goblin;
+    this.score = 0;
+    this.scoreDisplay = scoreDisplay;
+
+    this.initMonsterInterval();
+    this.initMonsterClick();
+  }
+
+  // Инициализация интервала для перемещения гоблина
+  initMonsterInterval() {
+    setInterval(() => {
+      // Установка гоблина в случайную позицию на игровом поле
+      const randomCell =
+        this.field.cells[Math.floor(Math.random() * this.field.cells.length)];
+      randomCell.appendChild(this.goblin.element);
+    }, 1000);
+  }
+
+  // Инициализация обработчика клика по гоблину
+  initMonsterClick() {
+    this.goblin.element.addEventListener("click", () => {
+      this.score += 1;
+      this.updateScoreDisplay();
+    });
+  }
+
+  // Обновление отображения счета
+  updateScoreDisplay() {
+    this.scoreDisplay.textContent = "Score: " + this.score;
+  }
+}
+
+// Ожидание загрузки DOM
+document.addEventListener("DOMContentLoaded", function () {
+  const cells = document.querySelectorAll(".cell");
+  const scoreDisplay = document.querySelector(".score");
+  const goblinElement = document.querySelector(".monster");
+
+  // Создание экземпляра гоблина
+  const goblin = new Goblin(goblinElement);
+  // Создание экземпляра поля
+  const field = new Field(cells);
+  // Создание экземпляра логики игры
+  const gameLogic = new GameLogic(field, goblin, scoreDisplay);
+  goblinElement.classList.remove("hidden");
+});
